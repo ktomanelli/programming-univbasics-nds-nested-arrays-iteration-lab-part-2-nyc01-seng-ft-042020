364a5293c8e6f4e306eb0b04f90a402818040492
Kyle Tomanelli <kyletomanelli@gmail.com>
2020-04-09 18:23:03 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -14,10 +14,10 @@ def find_min_in_nested_arrays(arr)
   newArr
 end
 
-def find_greater_pair(arr)
-  newArr = []
-  arr.length.times do |i|
-    arr[i][0]>arr[i][1] ? newArr.push(arr[i][0]): newArr.push(arr[i][1])
-  end
-  newArr.sort()
-end
+# def find_greater_pair(arr)
+#   newArr = []
+#   arr.length.times do |i|
+#     arr[i][0]>arr[i][1] ? newArr.push(arr[i][0]): newArr.push(arr[i][1])
+#   end
+#   newArr.sort()
+# end
