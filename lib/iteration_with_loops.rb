0bb8636f4bfef11356a211915e2713580346c68b
Kyle Tomanelli <kyletomanelli@gmail.com>
2020-04-09 17:56:13 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -3,3 +3,10 @@ def find_min_in_nested_arrays(src)
   # Produce a new Array that contains the smallest number of each of the nested arrays
 
 end
+
+def find_greater_pair(arr)
+  newArr = []
+  arr.length.times do |i|
+    arr[i][0]>arr[i][1] ? newArr.push(arr[i][0]):newArr.push(arr[i][1])
+  end
+end
