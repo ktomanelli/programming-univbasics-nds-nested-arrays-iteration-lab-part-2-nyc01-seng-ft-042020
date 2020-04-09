8435c20ebe799a2e2ddb8b9aebed21ea4b1ac204
Kyle Tomanelli <kyletomanelli@gmail.com>
2020-04-09 17:59:34 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -11,7 +11,7 @@ end
 def find_greater_pair(arr)
   newArr = []
   arr.length.times do |i|
-    arr[i][0]>arr[i][1] ? newArr.push(arr[i][0]):newArr.push(arr[i][1])
+    arr[i][0]>arr[i][1] ? newArr.push(arr[i][0]): newArr.push(arr[i][1])
   end
   newArr
 end
